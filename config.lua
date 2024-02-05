8e4f9093f353d728d9c3e59323125ac3aa922d19
hoangharry <tonminhhoang2006@gmail.com>
2024-02-05 08:12:06 +0700

    My first commit
    Add colorschemes

---
+++ config.lua
@@ -0,0 +1,16 @@
+-- My favorite nvim themes are: minimal, NeoSolarized, everforest, nordic
+lvim.colorscheme = "minimal"
+
+lvim.plugins = {
+  --#region Colorscheme
+  { "sainnhe/everforest" },
+  { "rmehri01/onenord.nvim" },
+  { "Yazeed1s/minimal.nvim" },
+  { "mellow-theme/mellow.nvim" },
+  { "rose-pine/neovim" },
+  { "fenetikm/falcon" },
+  { "AlexvZyl/nordic.nvim" },
+  { "shaunsingh/nord.nvim" },
+  { "Tsuzat/NeoSolarized.nvim" }
+  --#endregion
+}
